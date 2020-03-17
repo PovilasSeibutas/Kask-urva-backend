542e79484adaf3c2c42964d44a4b79f19cf862e5
Darius <dariuk.p@gmail.com>
2020-03-17 23:02:30 +0200

    Trying service

--- src/main/java/lt/daivospakalikai/academysurvey/Answer/Answer.java
+++ src/main/java/lt/daivospakalikai/academysurvey/Answer/Answer.java
@@ -13,6 +13,7 @@ import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+
 import lt.daivospakalikai.academysurvey.Question.Question;
 import lt.daivospakalikai.academysurvey.Survey.Survey;
 
@@ -20,96 +21,100 @@ import lt.daivospakalikai.academysurvey.Survey.Survey;
 @Table(name = "answer")
 public class Answer implements Serializable {
 
-  private static final long serialVersionUID = 1L;
-  @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
-  @Basic(optional = false)
-  @Column(name = "id")
-  private Integer id;
-  @Basic(optional = false)
-  @NotNull
-  @Size(min = 1, max = 45)
-  @Column(name = "answer")
-  private String answer;
-  @JoinColumn(name = "question_id", referencedColumnName = "id")
-  @ManyToOne(optional = false)
-  private Question questionId;
-  @JoinColumn(name = "survey_id", referencedColumnName = "id")
-  @ManyToOne(optional = false)
-  private Survey surveyId;
+    private static final long serialVersionUID = 1L;
 
-  public Answer() {
-  }
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "id")
+    private Integer id;
 
-  public Answer(Integer id) {
-    this.id = id;
-  }
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 45)
+    @Column(name = "answer")
+    private String answer;
 
-  public Answer(Integer id, String answer) {
-    this.id = id;
-    this.answer = answer;
-  }
+    @JoinColumn(name = "question_id", referencedColumnName = "id")
+    @ManyToOne(optional = false)
+    private Question questionId;
 
-  public Integer getId() {
-    return id;
-  }
+    @JoinColumn(name = "survey_id", referencedColumnName = "id")
+    @ManyToOne(optional = false)
+    private Survey surveyId;
 
-  public void setId(Integer id) {
-    this.id = id;
-  }
-
-  public String getAnswer() {
-    return answer;
-  }
-
-  public void setAnswer(String answer) {
-    this.answer = answer;
-  }
-
-  public Question getQuestionId() {
-    return questionId;
-  }
-
-  public void setQuestionId(Question questionId) {
-    this.questionId = questionId;
-  }
-
-  public Survey getSurveyId() {
-    return surveyId;
-  }
-
-  public void setSurveyId(Survey surveyId) {
-    this.surveyId = surveyId;
-  }
-
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) {
-      return true;
+    public Answer() {
     }
-    if (o == null || getClass() != o.getClass()) {
-      return false;
+
+    public Answer(Integer id) {
+        this.id = id;
     }
-    Answer answer1 = (Answer) o;
-    return Objects.equals(id, answer1.id) &&
-        Objects.equals(answer, answer1.answer) &&
-        Objects.equals(questionId, answer1.questionId) &&
-        Objects.equals(surveyId, answer1.surveyId);
-  }
 
-  @Override
-  public int hashCode() {
-    return Objects.hash(id, answer, questionId, surveyId);
-  }
+    public Answer(Integer id, String answer) {
+        this.id = id;
+        this.answer = answer;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+
+    public Question getQuestionId() {
+        return questionId;
+    }
+
+    public void setQuestionId(Question questionId) {
+        this.questionId = questionId;
+    }
+
+    public Survey getSurveyId() {
+        return surveyId;
+    }
+
+    public void setSurveyId(Survey surveyId) {
+        this.surveyId = surveyId;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Answer answer1 = (Answer) o;
+        return Objects.equals(id, answer1.id) &&
+                Objects.equals(answer, answer1.answer) &&
+                Objects.equals(questionId, answer1.questionId) &&
+                Objects.equals(surveyId, answer1.surveyId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, answer, questionId, surveyId);
+    }
 
 
-  @Override
-  public String toString() {
-    return "Answer{" +
-        "id=" + id +
-        ", answer='" + answer + '\'' +
-        ", questionId=" + questionId +
-        ", surveyId=" + surveyId +
-        '}';
-  }
+    @Override
+    public String toString() {
+        return "Answer{" +
+                "id=" + id +
+                ", answer='" + answer + '\'' +
+                ", questionId=" + questionId +
+                ", surveyId=" + surveyId +
+                '}';
+    }
 }
--- src/main/java/lt/daivospakalikai/academysurvey/Survey/SurveyRepository.java
+++ src/main/java/lt/daivospakalikai/academysurvey/Survey/SurveyRepository.java
@@ -1,7 +1,11 @@
 package lt.daivospakalikai.academysurvey.Survey;
 
+import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface SurveyRepository extends JpaRepository<Survey, Integer> {
+
+    @Query("select survey.* from survey JOIN answer ON answer.survey_id = survey.id JOIN question ON question.id = answer.question_id")
+    Survey getAllSurveysWithQuestionsAndAnswers();
 
 }
