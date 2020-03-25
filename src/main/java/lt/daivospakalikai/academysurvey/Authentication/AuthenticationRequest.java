e24f00b4256acd1e3abec173ae765cee167e8048
[Paulius K.] <[paulius.net@gmail.com]>
2020-03-25 15:18:31 +0200

    fix(setters): removed setters

--- src/main/java/lt/daivospakalikai/academysurvey/Authentication/AuthenticationRequest.java
+++ src/main/java/lt/daivospakalikai/academysurvey/Authentication/AuthenticationRequest.java
@@ -17,15 +17,8 @@ public class AuthenticationRequest {
         return username;
     }
 
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
     public String getPassword() {
         return password;
     }
 
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
