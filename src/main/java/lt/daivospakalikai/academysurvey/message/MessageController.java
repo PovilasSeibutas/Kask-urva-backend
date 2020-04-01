a4dd243af078b9a37dd67ba29636374dcad467ba
Povilas Seibutas <povilas.se@gmail.com>
2020-04-01 21:20:59 +0300

    fix(controllers): crossorigin added

--- src/main/java/lt/daivospakalikai/academysurvey/message/MessageController.java
+++ src/main/java/lt/daivospakalikai/academysurvey/message/MessageController.java
@@ -2,6 +2,7 @@ package lt.daivospakalikai.academysurvey.message;
 
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("/messages")
 public class MessageController {
 
